339b904ddd4c3c2468cb25631013557f6a4baf31
BluIsMe <soeun2388@kangnam.ac.kr>
2024-07-06 23:41:23 +0900

    first project ~2.3
    -Model 이용해서 변수값 web에 띄우기
    -한글 꺠짐 현상 수정 필요

--- src/main/java/com/example/springboot/controller/FirstController.java
+++ src/main/java/com/example/springboot/controller/FirstController.java
@@ -1,12 +1,19 @@
 package com.example.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username","soeun");
         return "greetings"; // greetings.mustache 파일로 반환값
+    }
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("username","soeun");
+        return "goodbye"; // greetings.mustache 파일로 반환값
     }
 }
